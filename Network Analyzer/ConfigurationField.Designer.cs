33ffb7551b719237d23d32c52496908a93e3e67f
Bo0m21 <Bo0m21@yandex.ru>
2019-06-18 22:26:00 +0400

    Изменил окно конфигурации

---
+++ Network Analyzer/ConfigurationField.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Network_Analyzer
+{
+    partial class ConfigurationField
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfigurationField));
+            this.gbGeleral = new System.Windows.Forms.GroupBox();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.lblSequenceType = new System.Windows.Forms.Label();
+            this.cbSequenceType = new System.Windows.Forms.ComboBox();
+            this.cbLength = new System.Windows.Forms.ComboBox();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblStartIndex = new System.Windows.Forms.Label();
+            this.tbStartIndex = new System.Windows.Forms.TextBox();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.lblInformation = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.gbGeleral.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbGeleral
+            // 
+            this.gbGeleral.Controls.Add(this.lblValue);
+            this.gbGeleral.Controls.Add(this.lblSequenceType);
+            this.gbGeleral.Controls.Add(this.cbSequenceType);
+            this.gbGeleral.Controls.Add(this.cbLength);
+            this.gbGeleral.Controls.Add(this.lblLength);
+            this.gbGeleral.Controls.Add(this.lblStartIndex);
+            this.gbGeleral.Controls.Add(this.tbStartIndex);
+            this.gbGeleral.Controls.Add(this.cbType);
+            this.gbGeleral.Controls.Add(this.lblType);
+            this.gbGeleral.Controls.Add(this.lblDescription);
+            this.gbGeleral.Controls.Add(this.tbDescription);
+            this.gbGeleral.Controls.Add(this.lblName);
+            this.gbGeleral.Controls.Add(this.tbName);
+            this.gbGeleral.Location = new System.Drawing.Point(12, 12);
+            this.gbGeleral.Name = "gbGeleral";
+            this.gbGeleral.Size = new System.Drawing.Size(360, 289);
+            this.gbGeleral.TabIndex = 0;
+            this.gbGeleral.TabStop = false;
+            this.gbGeleral.Text = "ConfigurationField.Geleral";
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblValue.Location = new System.Drawing.Point(6, 269);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(141, 15);
+            this.lblValue.TabIndex = 42;
+            this.lblValue.Text = "ConfigurationField.Value";
+            // 
+            // lblSequenceType
+            // 
+            this.lblSequenceType.AutoSize = true;
+            this.lblSequenceType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSequenceType.Location = new System.Drawing.Point(6, 98);
+            this.lblSequenceType.Name = "lblSequenceType";
+            this.lblSequenceType.Size = new System.Drawing.Size(192, 15);
+            this.lblSequenceType.TabIndex = 41;
+            this.lblSequenceType.Text = "ConfigurationField.SequenceType";
+            // 
+            // cbSequenceType
+            // 
+            this.cbSequenceType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSequenceType.FormattingEnabled = true;
+            this.cbSequenceType.Items.AddRange(new object[] {
+            "Little endian",
+            "Big endian"});
+            this.cbSequenceType.Location = new System.Drawing.Point(6, 116);
+            this.cbSequenceType.Name = "cbSequenceType";
+            this.cbSequenceType.Size = new System.Drawing.Size(348, 21);
+            this.cbSequenceType.TabIndex = 40;
+            this.cbSequenceType.SelectedIndexChanged += new System.EventHandler(this.CbSequenceType_SelectedIndexChanged);
+            // 
+            // cbLength
+            // 
+            this.cbLength.Enabled = false;
+            this.cbLength.FormattingEnabled = true;
+            this.cbLength.Location = new System.Drawing.Point(6, 241);
+            this.cbLength.Name = "cbLength";
+            this.cbLength.Size = new System.Drawing.Size(348, 21);
+            this.cbLength.TabIndex = 31;
+            this.cbLength.TextChanged += new System.EventHandler(this.CbLength_TextChanged);
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblLength.Location = new System.Drawing.Point(6, 223);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(148, 15);
+            this.lblLength.TabIndex = 30;
+            this.lblLength.Text = "ConfigurationField.Length";
+            // 
+            // lblStartIndex
+            // 
+            this.lblStartIndex.AutoSize = true;
+            this.lblStartIndex.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblStartIndex.Location = new System.Drawing.Point(6, 182);
+            this.lblStartIndex.Name = "lblStartIndex";
+            this.lblStartIndex.Size = new System.Drawing.Size(165, 15);
+            this.lblStartIndex.TabIndex = 29;
+            this.lblStartIndex.Text = "ConfigurationField.StartIndex";
+            // 
+            // tbStartIndex
+            // 
+            this.tbStartIndex.Location = new System.Drawing.Point(6, 200);
+            this.tbStartIndex.Name = "tbStartIndex";
+            this.tbStartIndex.Size = new System.Drawing.Size(348, 20);
+            this.tbStartIndex.TabIndex = 28;
+            this.tbStartIndex.TextChanged += new System.EventHandler(this.TbStartIndex_TextChanged);
+            // 
+            // cbType
+            // 
+            this.cbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Items.AddRange(new object[] {
+            "Types.Byte",
+            "Types.Sbyte",
+            "Types.Short",
+            "Types.Ushort",
+            "Types.Int",
+            "Types.Uint",
+            "Types.Long",
+            "Types.Ulong",
+            "Types.Float",
+            "Types.Double",
+            "Types.String"});
+            this.cbType.Location = new System.Drawing.Point(6, 158);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(348, 21);
+            this.cbType.TabIndex = 27;
+            this.cbType.SelectedIndexChanged += new System.EventHandler(this.CbType_SelectedIndexChanged);
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblType.Location = new System.Drawing.Point(6, 140);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(136, 15);
+            this.lblType.TabIndex = 26;
+            this.lblType.Text = "ConfigurationField.Type";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblDescription.Location = new System.Drawing.Point(6, 57);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(172, 15);
+            this.lblDescription.TabIndex = 24;
+            this.lblDescription.Text = "ConfigurationField.Description";
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.Location = new System.Drawing.Point(6, 75);
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.Size = new System.Drawing.Size(348, 20);
+            this.tbDescription.TabIndex = 23;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblName.Location = new System.Drawing.Point(6, 16);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(144, 15);
+            this.lblName.TabIndex = 22;
+            this.lblName.Text = "ConfigurationField.Name";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(6, 34);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(348, 20);
+            this.tbName.TabIndex = 21;
+            // 
+            // lblInformation
+            // 
+            this.lblInformation.AutoSize = true;
+            this.lblInformation.Location = new System.Drawing.Point(12, 333);
+            this.lblInformation.Name = "lblInformation";
+            this.lblInformation.Size = new System.Drawing.Size(146, 13);
+            this.lblInformation.TabIndex = 15;
+            this.lblInformation.Text = "ConfigurationField.Information";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(247, 307);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(125, 23);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "ConfigurationField.Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(12, 307);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(125, 23);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "ConfigurationField.Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // ConfigurationField
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 352);
+            this.Controls.Add(this.lblInformation);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.gbGeleral);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ConfigurationField";
+            this.Text = "ConfigurationField.Text";
+            this.Load += new System.EventHandler(this.ConfigurationField_Load);
+            this.gbGeleral.ResumeLayout(false);
+            this.gbGeleral.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbGeleral;
+        private System.Windows.Forms.Label lblInformation;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblStartIndex;
+        private System.Windows.Forms.TextBox tbStartIndex;
+        private System.Windows.Forms.ComboBox cbLength;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblSequenceType;
+        private System.Windows.Forms.ComboBox cbSequenceType;
+        private System.Windows.Forms.Label lblValue;
+    }
+}
---
+++ Network Analyzer/ConfigurationField.cs
@@ -0,0 +1,144 @@
+﻿using System.Windows.Forms;
+using Network_Analyzer.Extensions;
+using Network_Analyzer.Models.Connection;
+using Network_Analyzer.Services;
+
+namespace Network_Analyzer
+{
+    public partial class ConfigurationField : Form
+    {
+        private readonly ConnectionPacketModel m_PacketModel;
+        private readonly long m_StartIndex;
+
+        public ConfigurationField(ConnectionPacketModel packet, long startIndex)
+        {
+            InitializeComponent();
+            Localizer.LocalizeForm(this);
+
+            m_PacketModel = packet;
+            m_StartIndex = startIndex;
+        }
+
+        private void ConfigurationField_Load(object sender, System.EventArgs e)
+        {
+            if (m_StartIndex != -1)
+            {
+                tbStartIndex.Text = m_StartIndex.ToString();
+            }
+
+            cbSequenceType.SelectedIndex = 0;
+            cbType.SelectedIndex = 0;
+
+            lblInformation.Text = Localizer.LocalizeString("ConfigurationField.LoadedSuccessfully");
+        }
+
+        private void CbType_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            if ((string)cbType.SelectedItem == Localizer.LocalizeString("Types.String"))
+            {
+                cbLength.Enabled = true;
+            }
+            else
+            {
+                cbLength.Enabled = false;
+            }
+
+            UpdateValue();
+        }
+
+        private void CbSequenceType_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            UpdateValue();
+        }
+
+        private void TbStartIndex_TextChanged(object sender, System.EventArgs e)
+        {
+            UpdateValue();
+        }
+
+        private void CbLength_TextChanged(object sender, System.EventArgs e)
+        {
+            UpdateValue();
+        }
+
+        private void UpdateValue()
+        {
+            var reverse = cbSequenceType.SelectedIndex == 0 ? false : true;
+
+            if (!long.TryParse(tbStartIndex.Text, out long startindex))
+            {
+                lblInformation.Text = Localizer.LocalizeString("Editor.ErrorsStartIndex");
+                return;
+            }
+
+            if ((string)cbType.SelectedItem == Localizer.LocalizeString("Types.Byte"))
+            {
+                lblValue.Text = Localizer.LocalizeString("ConfigurationField.Value") + " " +
+                   m_PacketModel.Data.ReadByte((int)startindex);
+            }
+
+            if ((string)cbType.SelectedItem == Localizer.LocalizeString("Types.Sbyte"))
+            {
+                lblValue.Text = Localizer.LocalizeString("ConfigurationField.Value") + " " +
+                   m_PacketModel.Data.ReadSbyte((int)startindex);
+            }
+
+            if (m_StartIndex + 1 < m_PacketModel.Data.Length)
+            {
+                if ((string)cbType.SelectedItem == Localizer.LocalizeString("Types.Short"))
+                {
+                    lblValue.Text = Localizer.LocalizeString("ConfigurationField.Value") + " " +
+                        m_PacketModel.Data.ReadShort((int)startindex, reverse);
+                }
+
+                if ((string)cbType.SelectedItem == Localizer.LocalizeString("Types.Ushort"))
+                {
+                    lblValue.Text = Localizer.LocalizeString("ConfigurationField.Value") + " " +
+                       m_PacketModel.Data.ReadUshort((int)startindex, reverse);
+                }
+            }
+
+            if (m_StartIndex + 3 < m_PacketModel.Data.Length)
+            {
+                if ((string)cbType.SelectedItem == Localizer.LocalizeString("Types.Int"))
+                {
+                    lblValue.Text = Localizer.LocalizeString("ConfigurationField.Value") + " " +
+                        m_PacketModel.Data.ReadInt((int)startindex, reverse);
+                }
+
+                if ((string)cbType.SelectedItem == Localizer.LocalizeString("Types.Uint"))
+                {
+                    lblValue.Text = Localizer.LocalizeString("ConfigurationField.Value") + " " +
+                       m_PacketModel.Data.ReadUint((int)startindex, reverse);
+                }
+
+                if ((string)cbType.SelectedItem == Localizer.LocalizeString("Types.Float"))
+                {
+                    lblValue.Text = Localizer.LocalizeString("ConfigurationField.Value") + " " +
+                       m_PacketModel.Data.ReadFloat((int)startindex, reverse);
+                }
+            }
+
+            if (m_StartIndex + 7 < m_PacketModel.Data.Length)
+            {
+                if ((string)cbType.SelectedItem == Localizer.LocalizeString("Types.Long"))
+                {
+                    lblValue.Text = Localizer.LocalizeString("ConfigurationField.Value") + " " +
+                        m_PacketModel.Data.ReadLong((int)startindex, reverse);
+                }
+
+                if ((string)cbType.SelectedItem == Localizer.LocalizeString("Types.Ulong"))
+                {
+                    lblValue.Text = Localizer.LocalizeString("ConfigurationField.Value") + " " +
+                       m_PacketModel.Data.ReadUlong((int)startindex, reverse);
+                }
+
+                if ((string)cbType.SelectedItem == Localizer.LocalizeString("Types.Double"))
+                {
+                    lblValue.Text = Localizer.LocalizeString("ConfigurationField.Value") + " " +
+                       m_PacketModel.Data.ReadDouble((int)startindex, reverse);
+                }
+            }
+        }
+    }
+}
